a6113b05c1c433fe76d5a60fc413cb8a110e2fa8
Silva Neto <silva.neto.sistemas@gmail.com>
2019-07-07 11:36:05 -0300

    Criando repository e service para Categoria

--- src/main/java/com/silvaneto/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/silvaneto/cursomc/resources/CategoriaResource.java
@@ -1,29 +1,26 @@
 package com.silvaneto.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.silvaneto.cursomc.domain.Categoria;
+import com.silvaneto.cursomc.services.CategoriaService;
 
 @RestController
-@RequestMapping(value="/categorias")
+@RequestMapping(value = "/categorias")
 public class CategoriaResource {
-	
-	@GetMapping
-	public List<Categoria> listar () {
-		
-		Categoria c1 = new Categoria(1, "Informática");
-		Categoria c2 = new Categoria(2, "Escritório");
-		
-		List<Categoria> lista = new ArrayList<Categoria>();
-		lista.add(c1);
-		lista.add(c2);
-		
-		return lista;
+
+	@Autowired
+	private CategoriaService service;
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Categoria obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
---
+++ src/main/java/com/silvaneto/cursomc/services/CategoriaService.java
@@ -0,0 +1,20 @@
+package com.silvaneto.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.silvaneto.cursomc.domain.Categoria;
+import com.silvaneto.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+
+	public Categoria buscar(Integer id) {
+		Categoria obj = repo.findOne(id);
+		return obj;
+	}
+	
+}

7f8090673cc5423f64acd662085ac7b4ebc80f2f
Silva Neto <silva.neto.sistemas@gmail.com>
2019-07-07 12:00:11 -0300

    Criando operação de instânciação

--- src/main/java/com/silvaneto/cursomc/CursomcApplication.java
+++ src/main/java/com/silvaneto/cursomc/CursomcApplication.java
@@ -1,13 +1,31 @@
 package com.silvaneto.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.silvaneto.cursomc.domain.Categoria;
+import com.silvaneto.cursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria c1 = new Categoria(null, "Informática");
+		Categoria c2 = new Categoria(null, "Escritótio");
+		
+		categoriaRepository.save(Arrays.asList(c1, c2));
 	}
 
 }
